43beb75d1f124e18a6976dabe8f12ea26fa17a12
Joaoof <joaodeus400@gmail.com>
2024-02-06 17:03:44 -0300

    feat: Add Service LoggedUser

---
+++ RocketseatAuction.API/Services/LoggedUser.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Repositories;
+
+namespace RocketseatAuction.API.Services
+{
+    public class LoggedUser
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public LoggedUser(IHttpContextAccessor httpContext) 
+        {
+            _httpContextAccessor = httpContext;
+        } // Criar parâmetro -> forca construtor a receber parâmetro ->  
+
+        public User User()
+        {
+            var repository = new RocktseatAuctionDbContext();
+
+            var token = TokenOnRequest();
+            var email = FromBase64String(token);
+
+            return repository.Users.First(user => user.Email.Equals(email));        
+        }
+
+        public string TokenOnRequest()
+        {
+            var authentication = _httpContextAccessor.HttpContext!.Request.Headers.Authorization.ToString();
+
+            return authentication["Bearer ".Length..];
+        }
+
+        private string FromBase64String(string base64)
+        {
+            var data = Convert.FromBase64String(base64);
+
+            return System.Text.Encoding.UTF8.GetString(data);
+        }
+    }
+}
