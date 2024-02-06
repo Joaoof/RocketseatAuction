a1984f30e2b42513f61c7cf2d6a4ce25958c4445
Joaoof <joaodeus400@gmail.com>
2024-02-06 15:02:40 -0300

    feat: Checking if the token is valid

--- RocketseatAuction.API/Filters/AuthenticationUserAttribute.cs
+++ RocketseatAuction.API/Filters/AuthenticationUserAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Filters;
+using RocketseatAuction.API.Repositories;
 
 namespace RocketseatAuction.API.Filters
 {
@@ -8,6 +9,12 @@ namespace RocketseatAuction.API.Filters
         public void OnAuthorization(AuthorizationFilterContext context)
         {
             var token = TokenOnRequest(context.HttpContext);
+
+            var repository = new RocktseatAuctionDbContext();
+
+            var email = FromBase64String(token);
+
+            var exists = repository.Users.Any(user => user.Email.Equals(email));
         }
 
         private string TokenOnRequest(HttpContext context)
@@ -20,6 +27,13 @@ namespace RocketseatAuction.API.Filters
             }
 
             return authentication["Bearer ".Length..];
+        }
+
+        private string FromBase64String(string base64)
+        {
+            var data = Convert.FromBase64String(base64);
+
+            return System.Text.Encoding.UTF8.GetString(data);
         }
     }
 }
