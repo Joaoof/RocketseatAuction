4d85fefb320c433e6ef8b5eebd4256e2c199d0de
Joaoof <joaodeus400@gmail.com>
2024-02-06 11:04:23 -0300

    feat: Improving swagger documentation

--- RocketseatAuction.API/Controllers/AuctionController.cs
+++ RocketseatAuction.API/Controllers/AuctionController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RocketseatAuction.API.Entities;
 using RocketseatAuction.API.UsesCases.Auctions.GetCurrent;
 
 namespace RocketseatAuction.API.Controllers
@@ -8,12 +9,19 @@ namespace RocketseatAuction.API.Controllers
     public class AuctionController : ControllerBase
     {
         [HttpGet]
+        [ProducesResponseType(typeof(Auction), StatusCodes.Status200OK)] // Indicar para o swagger o que ele deve retornar!
+        [ProducesResponseType(StatusCodes.Status204NoContent)] // Caso o resultado no swagger seja null
         public IActionResult GetCurrentAuction() 
         {
 
             var useCase = new GetCurrentAuctionUseCase();
 
             var result = useCase.Execute();
+
+            if (result is null)
+            {
+                return NoContent();
+            }
 
             return Ok(result);
         }
