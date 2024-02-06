55e2baab195cfed6f97ad9fbd492941bb4294c66
Joaoof <joaodeus400@gmail.com>
2024-02-06 14:46:48 -0300

    feat: Add Authentication API Token endpoint

--- RocketseatAuction.API/Controllers/OfferController.cs
+++ RocketseatAuction.API/Controllers/OfferController.cs
@@ -1,15 +1,18 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RocketseatAuction.API.Comunication.Request;
+using RocketseatAuction.API.Filters;
 
 namespace RocketseatAuction.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [ServiceFilter(typeof(AuthenticationUserAttribute))] // criando autorização para esse endpoint 
     public class OfferController : RocketseatAuctionBaseController
     {
         [HttpPost]
         [Route("{itemId}")]
+     
         public IActionResult CreateOffer([FromRoute]int itemId, [FromBody] RequestCreateOfferJson request)
         {
             return Created();
---
+++ RocketseatAuction.API/Filters/AuthenticationUserAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace RocketseatAuction.API.Filters
+{
+    public class AuthenticationUserAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var token = TokenOnRequest(context.HttpContext);
+        }
+
+        private string TokenOnRequest(HttpContext context)
+        {
+            var authentication = context.Request.Headers.Authorization.ToString();
+
+            return authentication["Bearer ".Length..];
+        }
+    }
+}
