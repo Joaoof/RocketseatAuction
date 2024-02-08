ccd33fc06b1ce1119434599071920b992289bdc2
Joaoof <joaodeus400@gmail.com>
2024-02-08 16:16:47 -0300

    Implement flow for obtaining and displaying user information

---
+++ RocketseatAuction.API/Repositories/DataAccess/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using RocketseatAuction.API.Contracts;
+using RocketseatAuction.API.Entities;
+
+namespace RocketseatAuction.API.Repositories.DataAccess
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly RocktseatAuctionDbContext _dbContext;
+
+        public UserRepository(RocktseatAuctionDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public bool ExistUserWithEmail(string email)
+        {
+            return _dbContext.Users.Any(user => user.Email.Equals(email)); // Verificando se existir o email do token
+        }
+
+        public User GetUserByEmail(string email)
+        {
+            return _dbContext.Users.First(user => user.Email.Equals(email));
+        }
+    }
+}
