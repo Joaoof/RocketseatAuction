fdf49d0f3d2bcf2190e2bfeb0af826dfc5e9d901
Joaoof <joaodeus400@gmail.com>
2024-02-09 09:49:49 -0300

    fix: Fix root folder, because there was a bug

---
+++ Tests/UseCases.Test/Auctions/GetCurrent/GetCurrentAuctionUseCaseTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace UseCases.Test.Auctions.GetCurrent
+{
+    public class GetCurrentAuctionUseCaseTest
+    {
+        [Fact]
+        public void Sucess()
+        {
+            // ARRANGE 
+            var useCase = new GetCurrentAuctionUseCase();
+        }
+    }
+}
