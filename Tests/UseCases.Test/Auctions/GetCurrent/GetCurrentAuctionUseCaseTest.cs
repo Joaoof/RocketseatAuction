88e8158b10e1601b87ac0bda10c2e75d9c23a3f1
Joaoof <joaodeus400@gmail.com>
2024-02-09 10:24:28 -0300

    feat: Add Faker Data to my Test

--- Tests/UseCases.Test/Auctions/GetCurrent/GetCurrentAuctionUseCaseTest.cs
+++ Tests/UseCases.Test/Auctions/GetCurrent/GetCurrentAuctionUseCaseTest.cs
@@ -1,6 +1,9 @@
-﻿using FluentAssertions;
+﻿using Bogus;
+using FluentAssertions;
 using Moq;
 using RocketseatAuction.API.Contracts;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Enums;
 using RocketseatAuction.API.UsesCases.Auctions.GetCurrent;
 using Xunit;
 
@@ -11,8 +14,26 @@ namespace UseCases.Test.Auctions.GetCurrent
         [Fact]
         public void Sucess()
         {
+
+            var entity = new Faker<Auction>().RuleFor(auction => auction.Id, faker => faker.Random.Number(1, 10))
+                .RuleFor(auction => auction.Name, faker => faker.Lorem.Word())
+                .RuleFor(auction => auction.Starts, faker => faker.Date.Past())
+                .RuleFor(auction => auction.Ends, faker => faker.Date.Future())
+                .RuleFor(auction => auction.Items, (faker, auction) => new List<Item>
+                {
+                    new Item
+                    {
+                        Id = faker.Random.Number(1, 10),
+                        Name = faker.Commerce.ProductName(),
+                        Brand = faker.Commerce.Department(),
+                        BasePrice = faker.Random.Decimal(50, 1000),
+                        Condition = faker.PickRandom<Condition>(),
+                        AuctionId = auction.Id
+                    }
+                }).Generate();
+
             var mock = new Mock<IAuctionRepository>();
-            mock.Setup(i => i.GetCurrent()).Returns(new RocketseatAuction.API.Entities.Auction());
+            mock.Setup(i => i.GetCurrent()).Returns(entity);
             // ARRANGE 
             var useCase = new GetCurrentAuctionUseCase(mock.Object);
 
@@ -21,6 +42,8 @@ namespace UseCases.Test.Auctions.GetCurrent
 
             //ASSERT
             auction.Should().NotBeNull();
+            auction.Id.Should().Be(entity.Id);
+            auction.Name.Should().Be(entity.Name);
         }
     }
 }
